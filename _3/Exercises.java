cce68833ca5a06a699b4c2b3b81939403890f1a2
riccCic <127543799+riccCic@users.noreply.github.com>
2023-03-24 19:23:43 +0100

    Add files via upload

---
+++ _3/Exercises.java
@@ -0,0 +1,62 @@
+package javaoop.exercises._3;
+
+public class Exercises {
+    public static void main(String[] args) {
+        exercise1();
+        exercise2();
+    }
+
+    /**
+     * 1: Copy over the student class
+     *
+     *    Make sure there are 5 fields inside, if they are not simply create some more.
+     *    make every field private first.
+     *
+     *    Then create getters and setters for each of these 5 fields.
+     *
+     *    Once ready use all 5 below
+     */
+    private static void exercise1() {
+        System.out.println("Exercise 1:");
+
+        // Write and use your 5 getters and setters!
+        Studente studente = new Studente("Riccardo ", "Ciciriello, ", 25, " via c.monterosa,12");
+
+        System.out.println(studente.getName() + studente.getLastname() + "ha " + studente.getAge() + " anni, è abita in" + studente.getIndirizzo());
+        studente.setMaxAgeInCourse("Riccardo", 25);
+        studente.setAge(17);
+        studente.setName("Roberta ");
+        studente.setLastname("Rossi ");
+        studente.setIndirizzo(" via giuseppe verdi,15");
+
+        System.out.println(studente.getName() + studente.getLastname() + "ha " + studente.getAge() + " anni, è abita in" + studente.getIndirizzo());
+        studente.setMaxAgeInCourse("Roberta", 17);
+    }
+    /**
+     * 2: Create 3 setter constraints for the student class
+     *
+     *    Think about what reasonably constraints are?
+     *
+     *    Should a student be able to be a negative age?
+     *
+     *    Create a counter for one of the getters, try to think about
+     *    what other code could be inside a getter? Why not write it?
+     */
+    private static void exercise2() {
+        System.out.println("\nExercise 2:");
+
+        //Write your code here
+        Studente studente = new Studente("Riccardo ", "Ciciriello, ", 25, " via c.monterosa,12");
+        studente.StudenteVoto("Riccardo", 59);
+
+        studente.setName(" ");
+        studente.getName();
+
+
+
+
+    }
+
+}
+
+
---
+++ _3/Studente.java
@@ -0,0 +1,91 @@
+package javaoop.exercises._3;
+
+public class Studente {
+    private String name;
+    private String lastname;
+    private Integer age;
+    private String indirizzo;
+    private int maxAgeInCourse = 0;
+    private int votoSuperiori = 0;
+
+
+    public Studente(String name, String lastname, int age, String indirizzo) {
+        this.name = name;
+        this.lastname = lastname;
+        this.age = age;
+        this.indirizzo = indirizzo;
+
+    }
+
+    public Studente() {
+
+    }
+
+    public String getName() {
+        if (name == " ") {
+            System.out.println("questo studente non ha inserito il nome");
+        } else {
+            System.out.println("valido");
+        }
+        return name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getIndirizzo() {
+        return indirizzo;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setIndirizzo(String indirizzo) {
+        this.indirizzo = indirizzo;
+    }
+
+    public void setMaxAgeInCourse(String name, int maxAgeInCourse) {
+        if (this.age >= 18) {
+            this.maxAgeInCourse = maxAgeInCourse;
+            System.out.println("può frequentare il corso");
+        } else {
+            System.out.println("non può frequentare il corso");
+        }
+    }
+
+    public int getVotoSuperiori() {
+        return votoSuperiori;
+    }
+
+    public void setVotoSuperiori(int votoSuperiori) {
+        this.votoSuperiori = votoSuperiori;
+    }
+
+    public void StudenteVoto(String name, int votoSuperiori) {
+        this.votoSuperiori = votoSuperiori;
+        if (votoSuperiori < 60) {
+            System.out.println("non puoi essere ammesso perchè hai:" + votoSuperiori);
+        } else {
+            System.out.println("sei stato ammesso");
+        }
+
+    }
+
+}
+
+
+
